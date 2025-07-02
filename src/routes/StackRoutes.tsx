47fbeaf3a07115d9082e2dfc8ad6dd2c4e16b4a4
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-07-02 13:21:02 -0300

    Update screen titles

--- src/routes/StackRoutes.tsx
+++ src/routes/StackRoutes.tsx
@@ -1,5 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import { Form } from "@/app/Form/Form";
 import { Home } from "@/app/Home/Home";
@@ -10,45 +10,61 @@ import { Admin } from "@/app/Admin/Admin";
 import { Instructions } from "@/app/Instructions/Instructions";
 
 export type StackRoutesList = {
-    home: undefined
-    form: undefined
-    questions: undefined
-    score: { score: number }
-    users: undefined
-    admin: undefined
-    instructions: undefined
+  home: undefined;
+  form: undefined;
+  questions: undefined;
+  score: { score: number };
+  users: undefined;
+  admin: undefined;
+  instructions: undefined;
+};
 
+export type StackRoutesProps<T extends keyof StackRoutesList> =
+  NativeStackScreenProps<StackRoutesList, T>;
 
-}
-
-export type StackRoutesProps<T extends keyof StackRoutesList> = NativeStackScreenProps<StackRoutesList, T>
-
-const Stack = createNativeStackNavigator<StackRoutesList>()
+const Stack = createNativeStackNavigator<StackRoutesList>();
 
 export function StacksRoutes() {
-    return (
-        <Stack.Navigator initialRouteName="home" screenOptions={{
-            headerShown: false
-        }}>
-            <Stack.Screen name="home" component={Home} />
-            <Stack.Screen name="form" component={Form} options={{
-                headerShown: true
-            }} />
-            <Stack.Screen name="questions" component={Questions} />
-            <Stack.Screen name="score" component={Score} />
-            <Stack.Screen name="users" component={Users} options={{
-                headerShown: true
-            }} />
-            <Stack.Screen name="instructions" component={Instructions} options={{
-                headerShown: true
-            }} />
-            <Stack.Screen name="admin" component={Admin} options={{
-                headerShown: true
-            }} />
-
-
-
-        </Stack.Navigator>
-
-    )
+  return (
+    <Stack.Navigator
+      initialRouteName="home"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="home" component={Home} />
+      <Stack.Screen
+        name="form"
+        component={Form}
+        options={{
+          headerShown: true,
+          title: "Formulário",
+        }}
+      />
+      <Stack.Screen name="questions" component={Questions} />
+      <Stack.Screen name="score" component={Score} />
+      <Stack.Screen
+        name="users"
+        component={Users}
+        options={{
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="instructions"
+        component={Instructions}
+        options={{
+          headerShown: true,
+          title: "Regras",
+        }}
+      />
+      <Stack.Screen
+        name="admin"
+        component={Admin}
+        options={{
+          headerShown: true,
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
